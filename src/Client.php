75faa58b4fb38eca7e8001b467b22c8c9656444d
Rameez Hussain <rmz.hussain55@gmail.com>
2023-04-03 12:04:18 +0100

    -

--- src/Client.php
+++ src/Client.php
@@ -5,6 +5,7 @@ namespace Taz\AutoTraderStockClient;
 use Carbon\Carbon;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
@@ -28,20 +29,20 @@ class Client
 
     private function getToken(): string
     {
-        return Cache::lock(static::CACHE_KEY.'-lock')
+        return Cache::lock(static::CACHE_KEY . '-lock')
             ->block(10, function () {
                 return Cache::get(static::CACHE_KEY, function () {
                     $tokenResponse = Http::baseUrl(static::BASE_URL)
                         ->asForm()
                         ->post('authenticate', [
-                            'key'    => $this->key,
+                            'key' => $this->key,
                             'secret' => $this->secret,
                         ])
                         ->throw()
                         ->object();
                     $expiry = Carbon::parse($tokenResponse->expires);
 
-                    return Cache::remember(static::CACHE_KEY, $expiry, fn () => $tokenResponse->access_token);
+                    return Cache::remember(static::CACHE_KEY, $expiry, fn() => $tokenResponse->access_token);
                 });
             });
     }
@@ -58,8 +59,8 @@ class Client
     public function getVehicle(string $registration): VehicleDTO
     {
         $response = $this->request()
-            ->get('/service/stock-management/vehicles', [
-                'registration' => (string)Str::of($registration)->upper()->replace(' ', ''),
+            ->get('/vehicles', [
+                'registration' => $this->sanitizeRegistrationNumber($registration),
             ])
             ->throw()
             ->json();
@@ -70,7 +71,7 @@ class Client
     public function postVehicle(VehicleDTO $vehicle)
     {
         $response = $this->request()
-            ->post('/service/stock-management/stock', [
+            ->post('/stock', [
                 'vehicle' => $vehicle
             ])
             ->throw()
@@ -80,10 +81,52 @@ class Client
     public function updateVehicle(VehicleDTO $vehicle)
     {
         $response = $this->request()
-            ->patch('/service/stock-management/stock', [
+            ->patch('/stock', [
                 'vehicle' => $vehicle
             ])
             ->throw()
             ->object();
+    }
+
+    public function listVehicles(array $filters = []): Collection
+    {
+        $response = $this->request()
+            ->get('/stock', array_merge([
+                'vehicle' => 'false',
+                'advertiser' => 'false',
+                'adverts' => 'false',
+                'finance' => 'false',
+                'metadata' => 'false',
+                'features' => 'false',
+                'media' => 'false',
+                'responseMetrics' => 'false',
+                'check' => 'false',
+            ], $filters))
+            ->throw()
+            ->json();
+
+        return collect(Arr::get($response, 'vehicles'))
+            ->map(function ($vehicle) {
+                return new VehicleDTO($vehicle);
+            });
+    }
+
+    public function listVehiclesByReg(string $registration): VehicleDTO
+    {
+        return $this->listVehicles([
+            'registration' => $this->sanitizeRegistrationNumber($registration),
+        ])->first();
+    }
+
+    public function listVehiclesByVin(VehicleDTO $vehicleVin): VehicleDTO
+    {
+        return $this->listVehicles([
+            'vin' => $vehicleVin
+        ])->first();
+    }
+
+    public function sanitizeRegistrationNumber(string $registration): string
+    {
+        return (string)Str::of($registration)->upper()->replace(' ', '');
     }
 }
