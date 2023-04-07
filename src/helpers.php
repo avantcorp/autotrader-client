c592884c8cf5319e750f54733b70c19849c56f97
Ali Saleem <alisaleem@outlook.com>
2023-04-07 17:44:53 +0100

    registration validation

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace Taz\AutoTraderStockClient;
 
+use InvalidArgumentException;
+
 if (!function_exists('sanitize_registration')) {
     function sanitize_registration(string $registration): string
     {
-        return preg_replace('/[^A-Z0-9]/', '', strtoupper($registration));
+        $registration = preg_replace('/[^A-Z0-9]/', '', strtoupper($registration));
+
+        throw_unless(
+            2 <= strlen($registration) && strlen($registration) <= 7,
+            InvalidArgumentException::class
+        );
+
+        return $registration;
     }
 }
