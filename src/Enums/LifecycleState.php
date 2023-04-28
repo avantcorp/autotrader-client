0b9e5e2a9dea240d185c03bf4a7c8780ad224f60
Rameez <rmz.hussain55@gmail.com>
2023-04-28 12:00:24 +0100

    fix typo

--- src/Enums/LifecycleState.php
+++ src/Enums/LifecycleState.php
@@ -5,7 +5,7 @@ namespace Taz\AutoTraderStockClient\Enums;
 enum LifecycleState: string
 {
     case DueIn = 'DUE_IN';
-    case ForeCourt = 'FORECOURT';
+    case Forecourt = 'FORECOURT';
     case SaleInProgress = 'SALE_IN_PROGRESS';
     case Wastebin = 'WASTEBIN';
     case Deleted = 'DELETED';
