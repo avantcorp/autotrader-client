027c4dcbcaeae6b3a8fe1d5bb928105cdaa2ac08
Ali Saleem <alisaleem@outlook.com>
2023-05-25 14:50:01 +0100

    fix typo

--- src/Models/Stock.php
+++ src/Models/Stock.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Taz\AutoTraderStockClient\Models;
 
+use Illuminate\Support\Collection;
 use Taz\AutoTraderStockClient\Casts\CollectionCast;
 
 /**
- * @property Adverts $adverts
- * @property Vehicle $vehicle
- * @property Metadata $metadata
- * @property \Illuminate\Support\Collection<Feature>|Feature[] $features
- * @property Location $location
- * @property Media $media
- * @property Checks $checks
- * @property \Illuminate\Support\Collection<Message>|Message[] $messages
+ * @property Adverts                       $adverts
+ * @property Vehicle                       $vehicle
+ * @property Metadata                      $metadata
+ * @property Collection<Feature>|Feature[] $features
+ * @property Location                      $location
+ * @property Media                         $media
+ * @property Check                         $check
+ * @property Collection<Message>|Message[] $messages
  */
 class Stock extends Model
 {
@@ -24,7 +25,7 @@ class Stock extends Model
         'features' => CollectionCast::class.':'.Feature::class,
         'adverts'  => Adverts::class,
         'location' => Location::class,
-        'checks'   => Checks::class,
+        'check'    => Check::class,
         'media'    => Media::class,
         'messages' => CollectionCast::class.':'.Message::class,
     ];
