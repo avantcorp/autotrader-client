6ad1a9ea8f07e1ee7fbb1aa03ee78a22e59aea95
Ali Saleem <alisaleem@outlook.com>
2024-06-25 13:18:57 +0100

    Add 'Sold' cases to LifecycleState enum

--- src/Enums/LifecycleState.php
+++ src/Enums/LifecycleState.php
@@ -4,9 +4,10 @@ namespace Taz\AutoTraderStockClient\Enums;
 
 enum LifecycleState: string
 {
+    case Deleted = 'DELETED';
     case DueIn = 'DUE_IN';
     case Forecourt = 'FORECOURT';
     case SaleInProgress = 'SALE_IN_PROGRESS';
+    case Sold = 'SOLD';
     case Wastebin = 'WASTEBIN';
-    case Deleted = 'DELETED';
 }
